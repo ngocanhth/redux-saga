4eceb875f7432003aa761cd3336c6d0ed7d2dcc9
Ngoc Anh <anhln2@arrowhitech.net>
2022-05-02 11:58:23 +0700

    Ask fix not log root saga

---
+++ src/app/rootSaga.ts
@@ -0,0 +1,12 @@
+import counterSaga from 'features/counter/counterSaga';
+import {all} from 'redux-saga/effects';
+
+function* heloSaga() {
+    console.log('Hello Saga');
+}
+
+export default function* rootSaga() {
+    console.log("Root Saga");
+
+  //  yield all([heloSaga(), counterSaga() ]);
+}
