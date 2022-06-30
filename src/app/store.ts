1327609ca7f738f7c078df175701a9d38f633b8a
ngocanh <lengocanhxd@gmail.com>
2022-06-30 19:07:10 +0700

    Bai 4 - Add Redux Saga vào Redux Toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,6 +9,7 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  devTools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 });
 
--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,3 +1,20 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import { takeEvery, takeLatest } from "redux-saga/effects";
+import { increment } from "./counterSlice";
+
+export function* log(action: PayloadAction){
+    console.log('Action: ', action);
+}
+
+export function* log1(action: PayloadAction){
+    console.log('Action counter/increment with effect takeLatest: ', action);
+}
+
 export default function* counterSaga() {
     console.log("Counter Saga");
+
+   //  effect * nghia la listening any action
+    yield takeEvery('*', log);
+   // yield takeLatest('counter/increment', log1);
+   yield takeLatest(increment().type, log1);
 }
