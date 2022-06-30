c056ec6ae4834769a98fd986bed851f3934bbd12
ngocanh <lengocanhxd@gmail.com>
2022-06-30 22:19:42 +0700

    Phân biệt takeEvery và takeLatest

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -8,6 +8,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  incrementBySaga,
 } from './counterSlice';
 import styles from './Counter.module.css';
 
@@ -50,12 +51,20 @@ export function Counter() {
         >
           Add Amount
         </button>
-        <button
+        {/* <button
           className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
+        </button> */}
+
+        <button
+          className={styles.asyncButton}
+          onClick={() => dispatch(incrementBySaga(incrementValue))}
+        >
+          Add Async Saga
         </button>
+
         <button
           className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,6 +1,6 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { takeEvery, takeLatest } from "redux-saga/effects";
-import { increment } from "./counterSlice";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { increment, incrementBySaga, incrementBySagaSuccess } from "./counterSlice";
 
 export function* log(action: PayloadAction){
     console.log('Action: ', action);
@@ -10,11 +10,23 @@ export function* log1(action: PayloadAction){
     console.log('Action counter/increment with effect takeLatest: ', action);
 }
 
+export function* handleIncreasementSaga(action: PayloadAction<number>){
+    console.log("Waiting 2s");
+
+    yield delay(2000);
+
+    console.log("Waiting done, dispatch action");
+
+    yield put(incrementBySagaSuccess(action.payload));
+}
+
 export default function* counterSaga() {
     console.log("Counter Saga");
 
    //  effect * nghia la listening any action
     yield takeEvery('*', log);
    // yield takeLatest('counter/increment', log1);
-   yield takeLatest(increment().type, log1);
+ //  yield takeLatest(increment().type, log1);
+    yield takeEvery(incrementBySaga.toString(), handleIncreasementSaga);
+ //   yield takeLatest(incrementBySaga.toString(), handleIncreasementSaga);
 }
